02eeafd91cfeb4da98295ae77afadd5d0db4c06b
lenayee7 <lenayee07@gmail.com>
2020-04-17 20:30:01 -0700

    Run build for deployment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle,
+import { Card, CardImg, CardText, CardBody,
      Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label} from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
